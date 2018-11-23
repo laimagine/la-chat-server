dc02ec3da7f068e2c8f6539a68b3d23ce30a908b
laimagine <contact@laimagine.com>
2018-11-23 11:58:40 -0500

    Removed unused simple-peer.

--- index.ts
+++ index.ts
@@ -66,7 +66,7 @@ function onConnection(local: any): void {
   local.on('error', () => { onClose(local) });
   local.on('message', (data: any) => { onMessage(local, data) });
 
-  let response = JSON.stringify({ type: 'connected' });
+  const response = JSON.stringify({ type: 'connected' });
   console.log('onConnection response: ', response);
   local.send(response);
 
@@ -75,87 +75,93 @@ function onConnection(local: any): void {
 function onClose(local: any): void {
   connections[local.id] = null;
   if (local.remoteId) {
-    var remote = connections[local.remoteId];
+    const remote = connections[local.remoteId];
     remote.remoteId = null;
-    let remoteResponse = JSON.stringify({ type: 'close' });
+    const remoteResponse = JSON.stringify({ type: 'close' });
     console.log('onClose: remoteResponse: ', remoteResponse);
     remote.send(JSON.stringify(remoteResponse), onSend);
   }
 }
 
 function onMessage (local: any, data: any): void {
+
+  // States: register -> ready -> live -> call -> signal* -> hangup -> close
   try {
-    let message: any = JSON.parse(data);
+    const message: any = JSON.parse(data);
     console.log('onMessage: ', message);
 
     if (message.type === 'register') {
-      connections[message.localId] = local;
 
-      let localResponse: string = JSON.stringify({ type: 'ready' });
+      connections[message.localId] = local;
+      const localResponse: string = JSON.stringify({ type: 'ready' });
       console.log('register: localResponse: ', localResponse);
       local.send(localResponse, onSend);
 
     } else if (message.type === 'live') {
-      local.live = true;
+
       for (let key of Object.keys(connections)) {
-        let remote: any = connections[key];
-        if (remote.localId != message.localId) {
+        const remote: any = connections[key];
+        if (!remote) {
+          delete connections[key];
+        } else if (!remote.live && key !== message.localId) {
 
           console.log(`Connecting ${message.localId} and ${key}`);
-          local.remoteId = key;
-          let remote: any = connections[key];
-          remote.remoteId = message.localId;
     
-          let localResponse: string = JSON.stringify({ type: 'accept', data: { initiator: true }, remoteId: local.remoteId });
+          const localResponse: string = JSON.stringify({ type: 'call', remoteId: key, data: { initiator: true }});
           console.log('live: localResponse: ', localResponse);
           local.send(localResponse, onSend);
     
-          let remoteResponse: string = JSON.stringify({ type: 'accept', remoteId: remote.remoteId });
+          const remoteResponse: string = JSON.stringify({ type: 'call', remoteId: message.localId });
           console.log('live: remoteResponse: ', remoteResponse);
+
+          const remote: any = connections[key];
           remote.send(remoteResponse, onSend);
 
-          local.live = false;
-          remote.live = false;
+          local.live = true;
+          remote.live = true;
 
           break;
+
         }
       }
 
     } else if (message.type === 'accept') {
 
-      let acceptResponse: string = JSON.stringify({ type: 'accept', data: message.data });
+      const acceptResponse: string = JSON.stringify({ type: 'accept', data: message.data });
       console.log('accept: acceptResponse: ', acceptResponse);
-      let remote: any = connections[local.remoteId];
+      const remote: any = connections[message.remoteId];
       remote.send(acceptResponse);
 
     } else if (message.type === 'signal') {
 
-      let signalResponse: string = JSON.stringify({ type: 'signal', data: message.data });
+      const signalResponse: string = JSON.stringify({ type: 'signal', data: message.data });
       console.log('signal: signalResponse: ', signalResponse);
-      let remote: any = connections[local.remoteId];
+      const remote: any = connections[message.remoteId];
       remote.send(signalResponse);
 
     } else if (message.type === 'hangup') {
 
-      var remote = connections[local.remoteId];
-      remote.remoteId = null;
-      remote.send(JSON.stringify({ type: 'done-hangup' }), onSend);
-      local.remoteId = null;
+      const remote = connections[message.remoteId];
+      remote.send(JSON.stringify({ type: 'hangup' }), onSend);
+      local.send(JSON.stringify({ type: 'done-hangup' }), onSend);
 
-      let remoteResponse = JSON.stringify({ type: 'hangup' });
-      console.log('hangup remoteResponse: ', remoteResponse);
-      remote.send(remoteResponse);
+      local.live = false;
+      remote.live = false;
 
+    } else if (message.type === 'close') {
+      console.log('close: signalResponse: ', message.localId, message.remoteId);
     } else {
       console.error('unknown message `type` ' + message.type);
+      delete connections[message.localId];
+      delete connections[message.remoteId];
     }
   
   } catch (err) {
-    console.error('Discarding non-JSON message: ' + err);
+    console.error('Discarding non-JSON message: ' + err.stack);
   }
 
 }
 
 function onSend(err: any) {
-  if (err) console.log('onSend: ', err);
+  if (err) console.error('onSend: ', err);
 }
